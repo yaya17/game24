bcbc5b5c7a14a6de1b6912cefcdbcc1ea8d8182c
Sam558 <Sam558@users.noreply.github.com>
2018-01-06 19:52:02 -0600

    game of the day v0.0.0.1

---
+++ gameoftheday.js
@@ -0,0 +1,33 @@
+/// <reference path="C:\Joyce\Compute24\Compute24\Scripts/jquery-3.2.1.js" />
+
+var config = {
+    apiKey: "AIzaSyCTkLo8YEWP6pza2z44lZ417dp5YJD7v3I",
+    authDomain: "game24project.firebaseapp.com",
+    databaseURL: "https://game24project.firebaseio.com",
+    projectId: "game24project",
+    storageBucket: "game24project.appspot.com",
+    messagingSenderId: "767141719374"
+};
+var game24 = firebase.initializeApp(config);
+
+
+var gameoftheday;
+game24.database().ref('gameoftheday').on('value', function(snapshot) {
+    gameoftheday = snapshot.val();
+    $('#btnGOTD').show().css('color','blue');
+    //console.log(JSON.stringify(gameoftheday));
+});
+
+$('#btnGOTD').click(function(e){
+    if(gameoftheday)
+        $('#inputValues').val(gameoftheday)
+    $('#btnGOTD').hide();
+})
+
+function SaveGOTD(){
+    var gotd = $('#inputValues').val();
+    var json = JSON.parse('[' +gotd +']');
+    game24.database().ref('gameoftheday').set(json);
+}
+
+$('#btnSaveGOTD').click(SaveGOTD);
